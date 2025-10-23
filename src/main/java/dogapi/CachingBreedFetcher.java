5ff12a8c2999480f50055060d0c433fb4073a20f
Raymond Ye <raymondye45@gmail.com>
2025-10-23 15:52:44 -0400

    Implemented CachingBreedFetcher and fixed bug in DogApiBreedFetcher

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -15,14 +15,31 @@ import java.util.*;
 public class CachingBreedFetcher implements BreedFetcher {
     // TODO Task 2: Complete this class
     private int callsMade = 0;
-    public CachingBreedFetcher(BreedFetcher fetcher) {
+    private BreedFetcher fetcher;
+    private HashMap<String, List<String>> cache = new HashMap<>();
 
+    public CachingBreedFetcher(BreedFetcher fetcher) {
+        this.fetcher = fetcher;
     }
 
     @Override
     public List<String> getSubBreeds(String breed) {
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+
+        try{
+            if(cache.containsKey(breed)){
+                return cache.get(breed);
+            }
+            else{
+                callsMade ++;
+                List<String> sub_breeds = fetcher.getSubBreeds(breed);
+                cache.put(breed, sub_breeds);
+                return sub_breeds;
+            }
+
+        }catch(BreedNotFoundException e){
+            throw e;
+        }
     }
 
     public int getCallsMade() {
